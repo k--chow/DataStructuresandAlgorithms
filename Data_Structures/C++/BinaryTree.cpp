e736e320030a9ba67a547b031b38d0c06301ade0
kchow23 <kchow23@ufl.edu>
2016-05-01 17:08:04 -0400

    Working on BinaryTree still
    Add more methods, add traversal, add row by row traversal

--- Data_Structures/C++/BinaryTree.cpp
+++ Data_Structures/C++/BinaryTree.cpp
@@ -11,19 +11,90 @@
 #include <cstdlib>
 using namespace std;
 
-struct node
+struct Node
 {
     int value;
-    node *left;
-    node *right;
+    Node *left;
+    Node *right;
+    Node() : value(0), left(NULL), right(NULL){}
 };
 
 class BinaryTree
 {
+    private:
+        Node* root;
+    public:
+        BinaryTree() : root(NULL)
+        {
+
+        }
+
+        Node* find(int key)
+        {
+
+        }
+
+        void insert(int value)
+        {
+            Node* newNode = new Node;
+            newNode->value = value;
+            //check if root is null
+            if (root == NULL)
+            {
+                root = newNode;
+            }
+            else //traverse and insert
+            {
+                Node* current = root;
+                Node* parent; //needed
+                while (true)
+                {
+                    parent = current;
+                    if (value > current->value) //go right
+                    {
+                        current = current->right;
+
+                        if (current == NULL) //insert here
+                        {
+                            parent->right = newNode;
+                            return;
+                        }
+
+                    }
+                    else //go left
+                    {
+                        current = current->left;
+
+                        if (current == NULL)
+                        {
+                            parent->left = newNode;
+                            return;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        void traverse()
+        {
+
+        }
+
+        void displayTree()
+        {
+            cout << root->value << endl;
+            cout << root->left->value << endl;
+            cout << root->right->value << endl;
+        }
 
 };
 
 int main()
 {
-
+    BinaryTree Test;
+    Test.insert(5);
+    Test.insert(6);
+    Test.insert(4);
+    Test.displayTree();
 }
