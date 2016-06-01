0049bdac9636560a35c466acde22d1648b51eee1
kchow23 <kchow23@ufl.edu>
2016-06-01 07:35:38 -0400

    Working on quicksort

--- Algorithms/C++/quicksort.cpp
+++ Algorithms/C++/quicksort.cpp
@@ -9,8 +9,49 @@
 #include <string.h>
 using namespace std;
 
-class ArrayPartition
+void quick_sort(int leftIndex, int rightIndex, vector<int> & arr) //call itself
+{
+    //
+}
+
+//partition function
+int partitionIt(int left, int right, double pivot, vector<int> & arr) //splits array
 {
     //partition algorithm
+    //partition(int left, int right, array)
+    // find largest element to left of pivot
+    //find smallest element to right of pivot
+    //if left index is greater than right index, swap them
+    //return largest eleemnt to left of pivot
+}
 
-};
+double medianOfThree(int a, int b, int c) //returns median of 3 for pivot
+{
+    if (a > b)
+    {
+        if (b >= c) return b;
+        if (b < c) return c;
+    }
+    else if (b > a)
+    {
+        if (a >= c) return a;
+        if (a < c) return c;
+    }
+    else // b = a
+    {
+        return b;
+    }
+}
+
+//swap function
+void swapIt(int A, int B, vector<int> & arr) //swaps two elements
+{
+    int temp = arr[A];
+    arr[A] = arr[B];
+    arr[B] = temp;
+}
+
+int main()
+{
+
+}
