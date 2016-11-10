5d214029ca862f3bcb74f578823c5a1c9b98107b
kchow23 <kchow23@ufl.edu>
2016-11-09 23:59:01 -0500

    Tree Reconstruction

--- Algorithms/C++/TreeReconstruction.cpp
+++ Algorithms/C++/TreeReconstruction.cpp
@@ -11,7 +11,47 @@
 #include <queue>
 using namespace std;
 
+struct Node
+{
+    int value;
+    Node* left, right;
+    Node(int value): left(NULL), right(NULL)
+    {
+        this->value = value;
+        this->left = left;
+        this->right = right;
+    };
+
+};
+
+unordered_map<int, Node*> mappy;
+unordered_map<int, int> position;
+
 int main()
 {
-    
+    int nodes; cin >> nodes;
+    vector<int> postOrder;
+    vector<int> inOrder;
+    stack<int> s;
+    for(int i=0; i<nodes; i++)
+    {
+        int in; cin >> in;
+        s.push(in);
+    }
+    for(int i=0; i<nodes; i++)
+    {
+        int in; cin >> in;
+        mappy[in] = new Node(in);
+        position[in] = i;
+        postOrder.push_back(s.top());
+        s.pop();
+    }
+    Node* root = postOrder[0];
+    //traverse through
+    for(int i=1; i<nodes; i++)
+    {
+
+    }
+
+
 }
