8792b0e43239b19cfe0954618afd8eb4527a57b6
kchow23 <kchow23@ufl.edu>
2016-12-03 23:17:25 -0500

    Project

---
+++ Algorithms/C++/AGCA.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+
+//implementation of knuth-morris-pratt
+int KMP()
+{
+    return 0;
+}
+
+int main()
+{
+    string hint; cin >> hint;
+    int N; cin >> N;
+    vector<string> matrix;
+    for(int i=0; i<N; i++)
+    {
+        string given_string; cin >> given_string;
+        matrix.push_back(given_string);
+    }
+    vector<int> rows;
+    vector<int> cols;
+    //rows
+
+    //columns
+
+    //longest common subsequence
+
+}
